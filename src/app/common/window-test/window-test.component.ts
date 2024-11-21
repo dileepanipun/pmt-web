3befbdfeb280bd8cacf838cdda90667630891214
dileepa <dileepa9646@gmail.com>
2024-11-21 23:53:07 +0530

    feat: initialize Angular routing configuration
    - Add base routes configuration file
    - Set up empty routes array for future route definitions

---
+++ src/app/common/window-test/window-test.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-window-test',
+  imports: [],
+  templateUrl: './window-test.component.html',
+  styleUrl: './window-test.component.scss'
+})
+export class WindowTestComponent {
+
+}
