257d10532503fda1c0c94356bd551a037e3dbb5f
dileepa <dileepa9646@gmail.com>
2024-11-22 23:19:28 +0530

    feat(routing): add window-test and coming-soon routes
    - Add default route mapping to window-test component
    - Add coming-soon route for placeholder pages
    - Add catch-all route redirecting to coming-soon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,22 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {WindowTestComponent} from './common/window-test/window-test.component';
+import {CommingSoonPageComponent} from './temp/comming-soon-page/comming-soon-page.component';
 
-export const WINDOW_TEST_ROUTES: Routes = [
+export const routes: Routes = [
   {
     path: '',
     component: WindowTestComponent
+  },
+  {
+    path: 'window-test',
+    component: WindowTestComponent
+  },
+  {
+    path: 'coming-soon',
+    component: CommingSoonPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'coming-soon'
   }
 ];
